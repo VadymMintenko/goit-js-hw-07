2a9ee531f1f7f39b5a8a3b2722f79a74aa40a31b
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-18 17:26:27 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const addEl = document.createElement("ul");
+
+const elem = galleryItems.map(({ preview, original, description }) => {
+  const newItemEl = document.createElement("li");
+  const imageEl = document.createElement("img");
+  imageEl.src = `${preview}`;
+  imageEl.alt = `${description}`;
+  newItemEl.appendChild(imageEl);
+  return newItemEl;
+});
+
+addEl.append(...elem);
+galleryContainer.append(addEl);
+console.log(galleryContainer);
