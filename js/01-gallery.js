8a03b20dd1eabc406cb27fc056ba0b231c24c79a
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-18 19:21:24 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,17 +5,22 @@ console.log(galleryItems);
 
 const galleryContainer = document.querySelector(".gallery");
 
-const addList = document.createElement("ul");
+const itemElements = createItemEl(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", itemElements);
 
-const ItemElements = galleryItems.map(({ preview, original, description }) => {
-  const newItemEl = document.createElement("li");
-  const imageEl = document.createElement("img");
-  imageEl.src = `${preview}`;
-  imageEl.alt = `${description}`;
-  newItemEl.appendChild(imageEl);
-  return newItemEl;
-});
-
-addList.append(...ItemElements);
-galleryContainer.append(addList);
-console.log(galleryContainer);
+function createItemEl(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source="large-image.jpg"
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
