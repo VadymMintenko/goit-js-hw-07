94f9f0dce4bab7dadcc34078426a011f29c4e844
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-19 14:28:09 +0200

    commit-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,27 +1,31 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryContainer = document.querySelector(".gallery");
 
 const itemElements = createItemEl(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", itemElements);
+galleryContainer.addEventListener("click", clickImaage);
 
-function createItemEl(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
+function createItemEl(items) {
+  return items
+    .map((item) => {
       return `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src='${preview}'
-      data-source="large-image.jpg"
-      alt='${description}'
-    />
+  <a class="gallery__link" href="${item.original}">
+   <img class="gallery__image" data-source="${item.original}" data-pictureDesc="${item.description}" src="${item.preview}" alt="${item.description}" />
   </a>
 </div>`;
     })
     .join("");
 }
-console.log(basicLightbox);
+
+function clickImaage(evt) {
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  evt.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" alt="${evt.target.dataset.picturedesc}" />`
+  );
+  instance.show();
+}
